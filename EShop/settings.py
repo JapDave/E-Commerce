3ed157e9003ffb3b386779fc274d3fdd93ad24fc
jap <jap@aditetech.in>
2021-10-16 15:48:47 +0530

    updated app

--- EShop/settings.py
+++ EShop/settings.py
@@ -192,3 +192,6 @@ CELERY_TIMEZONE = 'Asia/Kolkata'
 
 USE_DJANGO_JQUERY = True
 
+import dj_database_url 
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)

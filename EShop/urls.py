177cc3552d5f0f0a82d7c2f6f0e4235ae36bb32d
jap <jap@aditetech.in>
2021-10-18 13:16:59 +0530

    updated app

--- EShop/urls.py
+++ EShop/urls.py
@@ -22,6 +22,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/', include('user.urls')),
+    path('', include('user.urls')),
     path('enterprise/',include('enterprise.urls'))
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)

d7f8f4cc88158263ef089b611946f283979d63b7
JaydeepJikadra <jips2598@gmail.com>
2021-10-19 16:28:32 +0530

    admin auth updated

--- eshop/urls.py
+++ eshop/urls.py
@@ -22,7 +22,7 @@ from enterprise.views import password_reset_request
 
 urlpatterns = [ 
     path('admin/', admin.site.urls),
-    path("password_reset",password_reset_request, name="admin_password_reset"),
+    path("password_reset",password_reset_request.as_view(), name="admin_password_reset"),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_confirm.html"), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),      

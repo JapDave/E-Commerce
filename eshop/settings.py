7c27642304229680d8185ae2426f67ff26237614
JaydeepJikadra <jips2598@gmail.com>
2021-10-19 16:55:27 +0530

    static config changed

--- eshop/settings.py
+++ eshop/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY=env("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['shopfreeapp.herokuapp.com']
 
@@ -150,7 +150,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = [(os.path.join(BASE_DIR, 'staticfiles'))]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
